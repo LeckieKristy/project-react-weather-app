195427b07f3be9e1d557fe2558ea53f2a7c73a90
LeckieKristy <bkris030@gmail.com>
2022-08-01 21:14:06 -0500

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -3,7 +3,17 @@ import "./App.css";
 export default function App() {
   return (
     <div className="WeatherApp">
-      <h1>Hello</h1>
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Kristy Leckie and is {""}{" "}
+        <a
+          href="https://github.com/LeckieKristy"
+          rel="noreferrer"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
