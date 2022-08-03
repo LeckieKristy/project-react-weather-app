780a57631fec513d34358b1e1b3e772f0d0b9894
LeckieKristy <bkris030@gmail.com>
2022-08-03 13:05:08 -0500

    Added Bootstrap
    added boostrap to the terminal

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import "./App.css";
+import React from "react";
 
 export default function App() {
   return (
     <div className="WeatherApp">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Kristy Leckie and is {""}{" "}
-        <a
-          href="https://github.com/LeckieKristy"
-          rel="noreferrer"
-          target="_blank"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="Container">
+        <h1>Weather App</h1>
+        <button className="btn btn-primary"> BUTTON</button>
+        <footer>
+          This project was coded by Kristy Leckie and is {""}{" "}
+          <a
+            href="https://github.com/LeckieKristy"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
