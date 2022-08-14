7f36e64b8846103b928471038f343fca78b7988b
LeckieKristy <100957443+LeckieKristy@users.noreply.github.com>
2022-08-13 20:38:38 -0600

    Changed some styling

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -3,7 +3,7 @@ import FormattedDate from "./FormattedDate";
 import WeatherTemperature from "./WeatherTemperature"
 
 export default function WeatherInfo(props){
-    return( <div> <h1>{props.data.city}</h1>
+    return( <div className="WeatherInfo"> <h1>{props.data.city}</h1>
     <ul>
       <li><FormattedDate date={props.data.date}></FormattedDate></li>
       <li className="text-uppercase">{props.data.description}</li>
